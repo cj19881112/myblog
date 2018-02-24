f3e68bbab4c548c73e8b34ac05d9b06e91ad5e4a
何锡捷 <397363287@qq.com>
2018-02-24 13:22:52 +0800

    初始化api项目

---
+++ code/myblog_api/src/main/java/com/cj/MyblogApiApplication.java
@@ -0,0 +1,12 @@
+package com.cj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogApiApplication.class, args);
+	}
+}
