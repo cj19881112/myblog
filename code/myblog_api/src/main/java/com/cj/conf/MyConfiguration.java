c4bc8092a88a2198c8316e74558527b4b50d95b8
何锡捷 <397363287@qq.com>
2018-02-25 13:04:46 +0800

    完成/api/kaptcha.jgp接口

--- code/myblog_api/src/main/java/com/cj/conf/MyConfiguration.java
+++ code/myblog_api/src/main/java/com/cj/conf/MyConfiguration.java
@@ -1,9 +1,58 @@
 package com.cj.conf;
 
+import javax.servlet.ServletException;
+
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.google.code.kaptcha.servlet.KaptchaServlet;
 
 @Configuration
 @MapperScan("com.cj.*.dao")
 public class MyConfiguration {
+	@Value("${kaptcha.border:no}")
+	private String border;
+
+	@Value("${kaptcha.textproducer.font.color:ref}")
+	private String fontColor;
+
+	@Value("${kaptcha.textproducer.font.size:43}")
+	private String fontSize;
+
+	@Value("${kaptcha.textproducer.font.names:Arial}")
+	private String fontName;
+
+	@Value("${kaptcha.image.width:135}")
+	private String width;
+
+	@Value("${kaptcha.image.height:50}")
+	private String height;
+
+	@Value("${kaptcha.textproducer.char.string:ACDEFHKPRSTWX345679}")
+	private String chars;
+
+	@Value("${kaptcha.noise.color:black}")
+	private String noiseColor;
+
+	@Value("${kaptcha.textproducer.char.length:4}")
+	private String charLength;
+
+	@Bean(name = "captchaProducer")
+	public ServletRegistrationBean servletRegistrationBean() throws ServletException {
+		ServletRegistrationBean servlet = new ServletRegistrationBean(new KaptchaServlet(), "/api/kaptcha.jpg");
+		servlet.addInitParameter("kaptcha.border", border);
+		servlet.addInitParameter("kaptcha.textproducer.font.color", fontColor);
+		servlet.addInitParameter("kaptcha.image.width", width);
+		servlet.addInitParameter("kaptcha.textproducer.char.string", chars);
+		servlet.addInitParameter("kaptcha.image.height", height);
+		servlet.addInitParameter("kaptcha.textproducer.font.size", fontSize);
+		servlet.addInitParameter("kaptcha.noise.color", noiseColor);
+		servlet.addInitParameter("kaptcha.textproducer.char.length", charLength);
+		servlet.addInitParameter("kaptcha.textproducer.font.names", fontName);
+		return servlet;
+	}
+
 }
--- code/myblog_api/src/test/java/com/cj/sys/SysConfigControllerIT.java
+++ code/myblog_api/src/test/java/com/cj/sys/SysConfigControllerIT.java
@@ -2,6 +2,8 @@ package com.cj.sys;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
@@ -11,11 +13,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.cj.RestHelper;
 import com.cj.util.ApiRet;
+import com.google.gson.Gson;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -24,6 +33,11 @@ public class SysConfigControllerIT {
 	@Autowired
 	private TestRestTemplate rest;
 
+	/**
+	 * 获取所有配置文件
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testGetSysConfig_returnAllConfig() throws Exception {
 		ResponseEntity<ApiRet<Map<String, String>>> ret = new RestHelper(rest).getForEntity("/api/sys/get_sys_conf",
@@ -31,5 +45,49 @@ public class SysConfigControllerIT {
 				});
 		assertThat(ret.getBody().code).isEqualTo(ApiRet.SUCC);
 		assertThat(ret.getBody().data.get("hello")).isEqualTo("world");
+	}
+
+	/**
+	 * 获取图片验证码
+	 */
+	@Test
+	public void testKaptcha_returnImg() {
+		ResponseEntity<byte[]> resp = requestKaptcha("");
+		assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(resp.getBody().length).isGreaterThan(0);
+	}
+
+	/**
+	 * 获取两次图片验证码不相同,因为没有controller，所以在这里测试边界
+	 */
+	@Test
+	public void testKaptcha_returnDifferentImg() {
+		ResponseEntity<byte[]> resp1 = requestKaptcha("1");
+		assertThat(resp1.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(resp1.getBody().length).isGreaterThan(0);
+
+		ResponseEntity<byte[]> resp2 = requestKaptcha("2");
+		assertThat(resp2.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(resp2.getBody().length).isGreaterThan(0);
+
+		assertThat(!Arrays.equals(resp1.getBody(), resp2.getBody())).isTrue();
+	}
+
+	/**
+	 * 获取图片验证码
+	 * 
+	 * @param r
+	 *            随机数
+	 * @return 图片的二级制数组
+	 */
+	private ResponseEntity<byte[]> requestKaptcha(String r) {
+		rest.getRestTemplate().getMessageConverters().add(new ByteArrayHttpMessageConverter());
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+		Map<String, Object> params = new HashMap<>();
+		params.put("_r", r);
+		HttpEntity<String> entity = new HttpEntity<String>(new Gson().toJson(params), headers);
+		ResponseEntity<byte[]> resp = rest.exchange("/api/kaptcha.jpg", HttpMethod.GET, entity, byte[].class);
+		return resp;
 	}
 }
